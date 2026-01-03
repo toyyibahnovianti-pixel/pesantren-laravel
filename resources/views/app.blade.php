77f7642b4f79fbf6092b901158328f4f52f79b2e
toyyibahnovianti-pixel <toyyibahnovianti@gmail.com>
2026-01-03 08:55:48 +0700

    Mengubah hero section menjadi sambutan pesantren

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,42 +9,30 @@
   <meta name="keywords" content="">
 
   <!-- Favicons -->
-  <link href="assets/img/favicon.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
+  <link href="{{ asset('assets/img/favicon.png') }}" rel="icon">
+  <link href="{{ asset('assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon"> 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com" rel="preconnect">
   <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
+  <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{ asset('assets/vendor/aos/aos.css') }}" rel="stylesheet">
   <!-- Main CSS File -->
-  <link href="assets/css/main.css" rel="stylesheet">
+  <link href="{{ ('assets/css/main.css') }}" rel="stylesheet">
 
-  <!-- =======================================================
-  * Template Name: Lumia
-  * Template URL: https://bootstrapmade.com/lumia-bootstrap-business-template/
-  * Updated: Aug 07 2024 with Bootstrap v5.3.3
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body class="index-page">
 
   <header id="header" class="header d-flex align-items-center sticky-top">
-    <div class="container-fluid position-relative d-flex align-items-center justify-content-between">
-
-      <a href="index.html" class="logo d-flex align-items-center me-auto me-xl-0">
-        <!-- Uncomment the line below if you also wish to use an image logo -->
-        <!-- <img src="assets/img/logo.png" alt=""> -->
-        <h1 class="sitename">Lumia</h1>
+    <div class="container-fluid position-relative d-flex align-items-center justify-content-between"> 
+      <a href="{{ url('/') }}" class="logo d-flex align-items-center me-auto me-xl-0">
+         <img src="{{ asset('assets/img/logo-pondok.png') }}" alt="Logo Pondok">
+  
+         <h1 class="sitename">Nama Pondok Anda</h1>
       </a>
 
       <nav id="navmenu" class="navmenu">
@@ -91,18 +79,23 @@
     <!-- Hero Section -->
     <section id="hero" class="hero section dark-background">
 
-      <img src="assets/img/hero-bg.jpg" alt="" data-aos="fade-in">
+  <img src="{{ asset('assets/img/logo-pondok.png.jpg') }}" alt="Background Pesantren" data-aos="fade-in">
 
-      <div class="container text-center" data-aos="fade-up" data-aos-delay="100">
-        <div class="row justify-content-center">
-          <div class="col-lg-8">
-            <h2>Welcome to Our Lumia</h2>
-            <p>We are team of talented designers making websites with Bootstrap</p>
-            <a href="#about" class="btn-get-started">Get Started</a>
-          </div>
+  <div class="container text-center" data-aos="fade-up" data-aos-delay="100">
+    <div class="row justify-content-center">
+      <div class="col-lg-8">
+        <h2>Selamat Datang di Pesantren al-insan</h2>
+        <p>Membentuk Generasi Qur'ani, Berakhlak Mulia, dan Unggul dalam Teknologi</p>
+        
+        <div class="d-flex justify-content-center gap-3">
+          <a href="{{ url('/daftar') }}" class="btn-get-started">Daftar Santri Baru</a>
+          <a href="#about" class="btn btn-outline-light px-4 py-2" style="border-radius: 50px; transition: 0.3s; margin-top: 30px;">Pelajari Profil</a>
         </div>
       </div>
+    </div>
+  </div>
 
+</section>
     </section><!-- /Hero Section -->
 
     <!-- What We Do Section -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,27 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
- <head>
-     <meta charset="utf-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-     <title>pesantren al-insan</title>
+@section('content')
 
+  <section id="hero" class="hero section dark-background">
+    <img src="{{ asset('assets/img/hero-bg.jpg') }}" alt="" data-aos="fade-in">
+    <div class="container text-center" data-aos="fade-up" data-aos-delay="100">
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <h2>Selamat Datang di Pesantren Al-Insan</h2>
+          <p>Mencetak Generasi Robbani yang Unggul dalam Imtaq dan Iptek</p>
+          <a href="{{ url('/daftar') }}" class="btn-get-started">Daftar Santri Baru</a>
+        </div>
+      </div>
+    </div>
+  </section>
 
-</head>
-      
-<body class="antialiased">
+  <section id="about" class="about section">
+    <div class="container" data-aos="fade-up">
+       <div class="section-title">
+          <h2>Tentang Al-Insan</h2>
+          <p>Pesantren kami berfokus pada pendidikan akhlak, tahfidz, dan penguasaan teknologi modern.</p>
+       </div>
+    </div>
+  </section>
 
-</body>
-
-</html>
+@endsection
